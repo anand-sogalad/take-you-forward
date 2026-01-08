d5e64640a2eda77bbdc65f037c8d380729721663
Anand Sogalad <anand.sogalad305@gmail.com>
2026-01-08 19:03:33 +0530

    Implement functions for determining the day of the week, summing first and last elements of an array, printing a value multiple times, and retrieving the last character of a string

--- src/take_you_forward/dsa/basics/language_basics/simple_problems.py
+++ src/take_you_forward/dsa/basics/language_basics/simple_problems.py
@@ -39,3 +39,53 @@ class SOlution:
             print("Grade D")
         else:
             print("Fail")
+
+    # 4. Given the integer day denoting the day number,
+    #     print on the screen which day of the week it is.
+    #     Week starts from Monday and
+    #     for values greater than 7 or less than 1, print Invalid.
+    #     Ensure only the 1st letter of the answer is capitalised.
+    def which_week_day(self, day: int):
+        match day:
+            case 1:
+                print("Monday")
+            case 2:
+                print("Tuesday")
+            case 3:
+                print("Wednesday")
+            case 4:
+                print("Thursday")
+            case 5:
+                print("Friday")
+            case 6:
+                print("Saturday")
+            case 7:
+                print("Sunday")
+            case _:
+                print("Invalid")
+
+    # 5. Sum of first last element in array
+    # Given an integer array nums, return the sum of the 1st and last element of the array.
+    def sum_of_first_and_last(self, nums: list[int]) -> int:
+        if len(nums) > 0:
+            return nums[0] + nums[-1]
+
+    # 5. Print X N numbers of times
+    #   Given two integers X and N, print the value X on the screen N times.
+    #   Separate each number by a single space.
+    #   Do not add a space after the last number.
+    #   After printing all N numbers, move to the next line.
+    #   If N = 0, still move to the next line (print an empty line).
+    def print_x(self, x: int, n: int):
+        for i in range(n):
+            if i < n - 1:
+                print(x, end=" ")
+            else:
+                print(x, end="")
+        print()
+
+    # 6. Print last character of string
+    #   Given a string s. Return the last character of the given string s.
+    def last_char(self, string: str):
+        if string:
+            return string[-1]
