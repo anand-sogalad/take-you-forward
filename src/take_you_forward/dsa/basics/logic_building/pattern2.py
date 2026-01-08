be0e32e0cb49428690bad0ba851eab0d37811c8c
Anand Sogalad <anand.sogalad305@gmail.com>
2026-01-08 21:39:06 +0530

    Add pattern generation script with two methods for printing asterisks

---
+++ src/take_you_forward/dsa/basics/logic_building/pattern2.py
@@ -0,0 +1,26 @@
+"""
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+*
+**
+***
+****
+*****
+"""
+
+
+class Solution:
+    def pattern2(self, n: int):
+        for i in range(n):
+            for _ in range(i + 1):
+                print("*", end="")
+            print()
+
+    def pattern2_2(self, n: int):
+        for i in range(1, n + 1):
+            print("*" * i)
+
+
+if __name__ == "__main__":
+    Solution().pattern2(5)
+    Solution().pattern2_2(5)
