adeaf043ba6a5de8a9cdcdce71b11a5bdded22b0
Anand Sogalad <anand.sogalad305@gmail.com>
2026-01-17 22:49:45 +0530

    Format code for consistency in reverse_array module

--- src/take_you_forward/dsa/basics/basic_arrays/reverse_array.py
+++ src/take_you_forward/dsa/basics/basic_arrays/reverse_array.py
@@ -5,12 +5,13 @@ Given an array arr of n elements. The task is to reverse the given array. The re
 
 class Solution(object):
     def reverse(self, arr: list[int]) -> list[int]:
-        left, right = 0, len(arr)-1
+        left, right = 0, len(arr) - 1
         while left < right:
             arr[left], arr[right] = arr[right], arr[left]
             left += 1
             right -= 1
         return arr
+
 
 if __name__ == "__name__":
     for i, j in (
