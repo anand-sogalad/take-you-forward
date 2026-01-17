facdebef3a81cf0c0f41ed44767328d2952b7ebc
Anand Sogalad <anand.sogalad305@gmail.com>
2026-01-17 22:47:36 +0530

    Add reverse_array module to reverse an array in place

---
+++ src/take_you_forward/dsa/basics/basic_arrays/reverse_array.py
@@ -0,0 +1,20 @@
+"""
+Given an array arr of n elements. The task is to reverse the given array. The reversal of array should be inplace.
+"""
+
+
+class Solution(object):
+    def reverse(self, arr: list[int]) -> list[int]:
+        left, right = 0, len(arr)-1
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+        return arr
+
+if __name__ == "__name__":
+    for i, j in (
+        ([1, 2, 3, 4, 5], [5, 4, 3, 2, 1]),
+        ([1, 2, 1, 1, 5, 1], [1, 5, 1, 1, 2, 1]),
+    ):
+        assert Solution().reverse(i) == j
