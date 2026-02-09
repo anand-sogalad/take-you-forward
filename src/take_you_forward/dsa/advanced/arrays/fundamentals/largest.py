707b6355c6472af907f75b0791d0f15e47310819
Anand Sogalad <anand.sogalad305@gmail.com>
2026-02-09 17:10:42 +0530

    Add largestElement module to return the largest element in an array

---
+++ src/take_you_forward/dsa/advanced/arrays/fundamentals/largest.py
@@ -0,0 +1,13 @@
+"""
+Given an array of integers nums, return the value of the largest element in the array
+"""
+
+
+class Solution:
+    def largestElement(self, nums):
+        largest = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] > largest:
+                largest = nums[i]
+        return largest
+
