0b299d2a7f476ede21146744874ad52ed7ac5c98
Anand Sogalad <anand.sogalad305@gmail.com>
2026-02-19 08:00:20 +0530

    add implementation for pattern19 in integer pattern generation

--- src/take_you_forward/roadmap/beginner_problems/patterns/easy_medium/pattern19.py
+++ src/take_you_forward/roadmap/beginner_problems/patterns/easy_medium/pattern19.py
@@ -17,3 +17,42 @@ Given an integer n. You need to recreate the pattern given below for any value o
 
 Print the pattern in the function given to you.
 """
+
+
+class Solution(object):
+    @staticmethod
+    def pattern19(n: int):
+
+        # upper
+        for i in range(n, 0, -1):
+            # stars
+            for j in range(i):
+                print("*", end="")
+
+            # spaces
+            for k in range((n - i) * 2):
+                print(" ", end="")
+
+            # stars
+            for l in range(i):
+                print("*", end="")
+            print()
+
+        # lower
+        for i in range(1, n + 1):
+            # stars
+            for j in range(i):
+                print("*", end="")
+
+            # spaces
+            for k in range((n - i) * 2):
+                print(" ", end="")
+
+            # stars
+            for l in range(i):
+                print("*", end="")
+            print()
+
+
+if __name__ == "__main__":
+    Solution.pattern19(5)
