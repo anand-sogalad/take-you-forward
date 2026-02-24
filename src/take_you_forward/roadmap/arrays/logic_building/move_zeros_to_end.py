6e628ad4e9bca813e3b1620786ce6ad92c3f499b
Anand Sogalad <104498976+anandsogalad15@users.noreply.github.com>
2026-02-24 11:02:16 +0530

    Refactor formatting and indentation in move_zeros_to_end function

--- src/take_you_forward/roadmap/arrays/logic_building/move_zeros_to_end.py
+++ src/take_you_forward/roadmap/arrays/logic_building/move_zeros_to_end.py
@@ -2,14 +2,17 @@
 Given an integer array nums, move all the 0's to the end of the array. The relative order of the other elements must remain the same.
 This must be done in place, without making a copy of the array.
 """
-def move_zeros_to_end(arr: list[int])-> list[int]:
-  left = 0
-  for i in range(len(arr)):
-    if arr[i] != 0:
-      arr[left], arr[i] = arr[i], arr[left]
-      left += 1
-  return arr 
+
+
+def move_zeros_to_end(arr: list[int]) -> list[int]:
+    left = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[left], arr[i] = arr[i], arr[left]
+            left += 1
+    return arr
+
 
 if __name__ == "__main__":
-  result = move_zeros_to_end([1, 2, 3, 0, 1, 0, 0, 4, 5])
-  print(result)
+    result = move_zeros_to_end([1, 2, 3, 0, 1, 0, 0, 4, 5])
+    print(result)
